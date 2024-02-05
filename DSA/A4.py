fe2b1c64eeba462ff9a45fa2f9c09e17e07a4137
khushal-patil <khushalhp2403@gmail.com>
2024-02-05 19:53:39 +0530

    initial prject version

---
+++ DSA/A4.py
@@ -0,0 +1,159 @@
+SetA = []
+SetB = []
+
+def insert():
+	n1 = int(input("Enter the number of elements in Set A: "))
+	for i in range(n1):
+		nm = int(input("Enter the element in Set A: "))
+		SetA.append(nm)
+		
+	n1 = int(input("Enter the number of elements in Set B: "))
+	for i in range(n1):
+		nm = int(input("Enter the element in Set B: "))
+		SetB.append(nm)
+		
+def display():
+	print("Set A : ",SetA)
+	print("Set B : ",SetB)
+	
+	
+def union():
+	res=[]
+	for i in SetA:
+		res.append(i)
+	for i in SetB:
+		if i not in res:
+			res.append(i)
+	
+	print("Union: ",res)
+
+def intersection():
+	res =[]
+	for i in SetA:
+		if i in SetB:
+			res.append(i)
+			
+	print("Intersection: ",res)
+
+def difference():
+	res =[]
+	
+	for i in SetA:
+		if i not in SetB:
+			res.append(i)
+			
+	for i in SetB:
+		if i not in SetA:
+			res.append(i)
+	print("Difference: ",res)
+	
+	
+def find():
+	t = int(input("1. Set A or 2. Set B: "))
+	s=False
+	s = int(input("Enter Element to Search: "))
+	if t==1:
+		for i in range(len(SetA)):
+			if s == SetA[i]:
+				s = True
+		if s == True:
+			print("Element Found...")
+		else:
+			print("Element Not Found...")
+	elif t==2:
+		for i in range(len(SetB)):
+			if s == SetB[i]:
+				s = True
+		if s == True:
+			print("Element Found...")
+		else:
+			print("Element Not Found...")
+
+def remove():
+	t = int(input("1. Set A or 2. Set B: "))
+	s=False
+	s1 = int(input("Enter Element to Deleted: "))
+	if t==1:
+		for i in range(len(SetA)):
+			if s1 == SetA[i]:
+				s = True
+		if s == True:
+			print("Element Found...")
+			SetA.remove(s1)
+			print("After Deletion",SetA)
+		else:
+			print("Element not in Set A.")
+	elif t==2:
+		for i in range(len(SetB)):
+			if s1 == SetB[i]:
+				s = True
+		if s == True:
+			print("Element Found...")
+			SetB.remove(s1)
+			print("After Deletion",SetB)
+		else:
+			print("Element not in Set B.")
+		
+def size():
+	ct=0
+	for i in SetA:
+		ct+=1
+	print("Size of SetA: ",ct)
+	ct=0
+	for i in SetB:
+		ct+=1
+	print("Size of SetB: ",ct)
+	
+def subset():
+	set5 = []
+	flag=0
+	for i in SetA:
+		if i in SetB:
+			set5.append(i)
+			flag=1
+		
+	if flag==1:
+		print("Subset",set5)
+		print("SetB is a Subset of SetA.")
+	else:
+		print("SetB is not Subset of SetA.")
+	
+while True:
+	print("******* Set Operations ******")
+	print("1. Insert")
+	print("2. Display")
+	print("3. Union ")
+	print("4. Intersection")
+	print("5. Difference")
+	print("6. Size of Sets")
+	print("7. Find ")
+	print("8. Delete an Element")
+	print("9. Subset")
+	print("0. Exit")
+	
+	ch = int(input("Enter the Choice: "))
+	
+	if ch==1:
+		insert()
+	elif ch==2:
+		display()
+	elif ch==3:
+		union()
+	elif ch==4:
+		intersection()
+	elif ch==5:
+		difference()
+	elif ch==6:
+		size()
+	elif ch==7:
+		find()
+	elif ch==8:
+		remove()
+	elif ch==9:
+		subset()
+	elif ch==0:
+		break
+	else:
+		print("Invalid Choice...")
+
+
---
+++ DSA/B5.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+
+struct node{
+	string label;
+	int ch_count;
+	
+	struct node* child[10];
+	
+}*root;
+
+class GT{
+
+	public:
+		
+		GT(){
+			
+			root = NULL;
+		}
+		
+		string lbel;
+		int count;
+		
+		void create(){
+		
+				
+				root = new node;
+				
+				cout<<"Enter the Name of Book: ";
+				cin>>root->label;
+				cout<<"Enter No. of Chapters: ";
+				cin>>root->ch_count;
+					
+				
+				for(int i=0;i<root->ch_count;i++){
+				
+					root->child[i] = new node;
+					cout<<"Enter the "<< i+1 <<" Name of Chapter: ";
+					cin>>root->child[i]->label;
+					cout<<"Enter the No of Sections: ";
+					cin>>root->child[i]->ch_count;
+
+					for(int j=0;j<root->child[i]->ch_count;j++){
+						root->child[i]->child[j] = new node;
+						cout<<"Enter the "<< i+1 <<" - "<< j+1<< " Name of Sections: ";
+						cin>>root->child[i]->child[j]->label;
+						cout<<"Enter the No of Sub-Section: ";
+						cin>>root->child[i]->child[j]->ch_count;
+						
+						for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
+							root->child[i]->child[j]->child[k] = new node;
+							cout<<"Enter the "<< i+1 <<" - "<< j+1<< " - "<< k+1<< " Name of Sub Section: ";
+							cin>>root->child[i]->child[j]->label;
+						
+						}
+					}
+					
+					
+				}
+			
+			
+		} 
+		
+		void display(node * r){
+		
+				cout<<endl;
+				cout<<"Name of Book: ";
+				cout<<root->label<<endl;
+				cout<<"No. of Chapters: ";
+				cout<<root->ch_count<<endl;
+			
+				for(int i=0;i<root->ch_count;i++){
+					
+						cout<<endl;
+						cout<<" Name of Chapter: ";
+						cout<<root->child[i]->label<<endl;
+						cout<<"	No of Sections: ";
+						cout<<root->child[i]->ch_count<<endl;
+
+						for(int j=0;j<root->child[i]->ch_count;j++){
+							
+							cout<<"\t\t"<< i+1 <<" - "<< j+1<< " Name of Sections: ";
+							cout<<root->child[i]->child[j]->label<<endl;
+							cout<<"\t\tNo of Sub-Section: ";
+							cout<<root->child[i]->child[j]->ch_count<<endl;
+							
+							for(int k=0;k<root->child[i]->child[j]->ch_count;k++){
+								
+								cout<<"\t\t\t"<< i+1 <<" - "<< j+1<< " - "<< k+1<< " Name of Sub Section: ";
+								cout<<root->child[i]->child[j]->label<<endl;
+							
+							}
+						}
+				
+			}
+		
+		}
+
+
+};
+
+
+int main(){
+	GT g;
+	
+	while(1){
+		cout<<"******* Book Information *******"<<endl;
+		cout<<"1. Add Book Info."<<endl;
+		cout<<"2. Display."<<endl;
+		cout<<"3. Exit"<<endl;
+		cout<<"Enter Choice: ";
+		int ch;
+		cin>>ch;
+
+		switch(ch){
+			case 1:
+				g.create();
+				break;
+			case 2:
+				g.display(root);
+				break;
+			case 3:
+				exit(0);
+				break;
+			default:
+				cout<<"Invalid Choice....";
+				break;
+		}
+	}
+}
