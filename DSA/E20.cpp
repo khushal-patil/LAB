f5edf53382da88fcec4650bd45b41bfaa08b87a0
khushal-patil <khushalhp2403@gmail.com>
2024-04-29 18:00:34 +0530

    updated

--- DSA/E20.cpp
+++ DSA/E20.cpp
@@ -1,8 +1,3 @@
-/*Consider a scenario for Hospital to cater services to different kinds of
-patients as Serious (top priority), b) non-serious (medium priority), c) General
-Checkup (Least priority). Implement the priority queue to cater services to the
-patients.*/
-
 #include <iostream>
 #define MAX_SIZE 10
 
