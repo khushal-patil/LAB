aee774b5233cf0f2996ecc7c9ab5c0d8ad7ce0d1
khushal-patil <khushalhp2403@gmail.com>
2024-07-21 22:48:26 +0530

    updated

--- 5TH SEMESTER/LB-I/FCFS.java
+++ 5TH SEMESTER/LB-I/FCFS.java
@@ -6,32 +6,22 @@ public class FCFS {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Enter No. of Processes: ");
-
     int n = sc.nextInt();
 
     int pno[] = new int[n];
-
     int ar[] = new int[n];
-
     int bt[] = new int[n];
-
     int ct[] = new int[n];
-
     int ta[] = new int[n];
-
     int wt[] = new int[n];
-
     int temp;
-
     float avgwt = 0;
 
     for (int i = 0; i < n; i++) {
       System.out.println("Enter Process " + (i + 1) + " Arrival Time: ");
-
       ar[i] = sc.nextInt();
 
       System.out.println("Enter Process " + (i + 1) + " Burst Time: ");
-
       bt[i] = sc.nextInt();
 
       pno[i] = i + 1;
@@ -41,21 +31,13 @@ public class FCFS {
       for (int j = 0; j < n - (i + 1); j++) {
         if (ar[j] > ar[j + 1]) {
           temp = ar[j];
-
           ar[j] = ar[j + 1];
-
           ar[j + 1] = temp;
-
           temp = bt[j];
-
           bt[j] = bt[j + 1];
-
           bt[j + 1] = temp;
-
           temp = pno[j];
-
           pno[j] = pno[j + 1];
-
           pno[j + 1] = temp;
         }
       }
@@ -67,75 +49,46 @@ public class FCFS {
       } else {
         if (ar[i] > ct[i - 1]) {
           ct[i] = ar[i] + bt[i];
-        } else ct[i] = ct[i - 1] + bt[i];
+        } else
+          ct[i] = ct[i - 1] + bt[i];
       }
 
       ta[i] = ct[i] - ar[i];
-
       wt[i] = ta[i] - bt[i];
-
       avgwt += wt[i];
     }
 
-    System.out.println(
-      "\n P.No. \t Arrival Time \t Burst time \t Service time \t Waiting time"
-    );
+    System.out.println("\n P.No. \t Arrival Time \t Burst time \t Service time \t Waiting time");
 
     for (int i = 0; i < n; i++) {
-      System.out.println(
-        "P" +
-        pno[i] +
-        " \t " +
-        ar[i] +
-        "\t\t" +
-        bt[i] +
-        "\t\t" +
-        ct[i] +
-        "\t\t" +
-        wt[i]
-      );
+      System.out.println("P" + pno[i] + " \t " + ar[i] + "\t\t" + bt[i] + "\t\t" + ct[i] + "\t\t" + wt[i]);
     }
 
     sc.close();
 
     System.out.println("");
-
     System.out.println("Gantt Chart : ");
 
-    for (int i = 0; i < n; i++) {
-      int c = 0;
+    for (int i = 1; i <= n; i++) {
+      System.out.print("----------------");
 
-      for (int j = 0; j < ct[i]; j++) {
-        if (c == 0) {
-          System.out.print("|____P" + pno[i] + " _");
-
-          c = 1;
-        } else {
-          System.out.print("_");
-        }
-      }
-
-      System.out.print(" |");
     }
-
-    System.out.println("");
-
+    System.out.print("\n");
     for (int i = 0; i < n; i++) {
-      int c = 0;
+      System.out.print("|\tP" + pno[i] + "\t");
 
-      for (int j = 0; j < ct[i]; j++) {
-        if (c == 0) {
-          System.out.print("\t" + ct[i]);
-
-          c = 1;
-        } else {
-          System.out.print(" ");
-        }
-      }
-
-      System.out.print(" |");
     }
+    System.out.print("|\n");
+    for (int i = 1; i <= n; i++) {
+      System.out.print("----------------");
 
+    }
+    System.out.print("\n");
+    System.out.print(ar[0]);
+    for (int i = 0; i < n; i++) {
+      System.out.print("\t\t" + ct[i]);
+
+    }
     System.out.println("\n\n Average Waiting Time : " + (avgwt / n));
   }
 }
